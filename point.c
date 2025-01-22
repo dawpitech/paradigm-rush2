e0576fc6a1e48afd0af733ef57651586f9096a6e
etienne <etienne.labarbe@epitech.eu>
2025-01-22 12:00:50 +0100

    feat(ex02): add and sub features

--- point.c
+++ point.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "point.h"
+#include "new.h"
 
 typedef struct
 {
@@ -27,19 +28,37 @@ static void Point_ctor(PointClass *this, va_list *args)
 
 static int my_intlen(int nbr)
 {
+    if (nbr < 0)
+        return 1 + my_intlen(-nbr);
     if (nbr < 10)
         return 1;
-    return 1 + nbr / 10; 
+    return 1 + my_intlen(nbr / 10); 
 }
 
 static char *Point_str(PointClass *this)
 {
-    int length = my_intlen(this->x) + my_intlen(this->y) + 14 + 1;
+    int length = my_intlen(this->x) + my_intlen(this->y) + 12 + 1;
     char *str = malloc(length);
 
-    if (snprintf(str, length, "<Vertex (%d, %d)>", this->x, this->y) == -1)
+    if (snprintf(str, length, "<Point (%d, %d)>", this->x, this->y) == -1)
         return NULL;
     return str;
+}
+
+static Object *Point_add(const PointClass *a, const PointClass *b)
+{
+    if (!a || !b)
+        raise("Error");
+
+    return new(Point, a->x + b->x, a->y + b->y);
+}
+
+static Object *Point_sub(const PointClass *a, const PointClass *b)
+{
+    if (!a || !b)
+        raise("Error");
+
+    return new(Point, a->x - b->x, a->y - b->y);
 }
 
 static const PointClass _description = {
@@ -49,8 +68,8 @@ static const PointClass _description = {
         .__ctor__ = (ctor_t)&Point_ctor,
         .__dtor__ = NULL,   /* Not needed atm */
         .__str__ = (to_string_t)&Point_str,    /* Implement this method for exercice 02 */
-        .__add__ = NULL,    /* Implement this method for exercice 03 */
-        .__sub__ = NULL,    /* Implement this method for exercice 03 */
+        .__add__ = (binary_operator_t)&Point_add,    /* Implement this method for exercice 03 */
+        .__sub__ = (binary_operator_t)&Point_sub,    /* Implement this method for exercice 03 */
         .__mul__ = NULL,
         .__div__ = NULL,
         .__eq__ = NULL,
