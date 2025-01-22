f0badd12746872346233b63bd545a5cc63a62c98
Antoine <me@antoinebellanger.fr>
2025-01-22 15:51:53 +0100

    refacto(ex04): Char class

--- char.c
+++ char.c
@@ -6,93 +6,89 @@
 */
 
 #include <stdio.h>
+
 #include "char.h"
 #include "new.h"
 
 typedef struct
 {
-    Class   base;
-    int c;
+    Class base;
+    char chr;
 }   CharClass;
 
 static void Char_ctor(CharClass *this, va_list *args)
 {
-    if (!args)
-        raise("Called Char constructor with missing arguments");
-    this->c = va_arg(*args, int);
+    if (this == NULL)
+        raise("Invalid char object passed to constructor");
+    if (args == NULL)
+        raise("Called char constructor with missing arguments");
+    this->chr = (char) va_arg(*args, int);
 }
 
 static char *Char_str(CharClass *this)
 {
-    int length = sizeof(char) + 9 + 1;
-    char *str = malloc(length);
+    int length = 11;
+    char *str;
 
-    if (snprintf(str, length, "<Char (%c)>", this->c) == -1)
+    if (this == NULL)
+        raise("Invalid char object passed to str");
+    str = calloc(1, length);
+    if (snprintf(str, length, "<Char (%c)>", this->chr) == -1)
         return NULL;
     return str;
 }
 
-static Object *Char_add(CharClass *this, CharClass *c)
+static Object *Char_add(const CharClass *a, const CharClass *b)
 {
-    if (!this || !c)
-        raise("Invalid argument");
-    return new(
-        Char,
-        this->c + c
-    );
+    printf("add\n");
+    if (a == NULL || b == NULL)
+        raise("Invalid char objects passed to add");
+    return new(Char, a->chr + b->chr);
 }
 
-static Object *Char_sub(CharClass *this, CharClass *c)
+static Object *Char_sub(const CharClass *a, const CharClass *b)
 {
-    if (!this || !c)
-        raise("Invalid argument");
-    return new(
-        Char,
-        this->c - c->c
-    );
+    if (a == NULL || b == NULL)
+        raise("Invalid char objects passed to sub");
+    return new(Char, a->chr - b->chr);
 }
 
-static Object *Char_mul(CharClass *this, CharClass *c)
+static Object *Char_mul(const CharClass *a, const CharClass *b)
 {
-    if (!this || !c)
-        raise("Invalid argument");
-    return new(
-        Char,
-        this->c * c->c
-    );
+    if (a == NULL || b == NULL)
+        raise("Invalid char objects passed to mul");
+    return new(Char, a->chr * b->chr);
 }
 
-static Object *Char_div(CharClass *this, CharClass *c)
+static Object *Char_div(const CharClass *a, const CharClass *b)
 {
-    if (!this || !c)
-        raise("Invalid argument");
-    if (c->c == 0)
-        raise("Cannot divide by 0!");
-    return new(
-        Char,
-        this->c / c->c
-    );
+    printf("div\n");
+    if (a == NULL || b == NULL)
+        raise("Invalid char objects passed to div");
+    if (b->chr == 0)
+        raise("Trying to divide by a char zero");
+    return new(Char, (char) (a->chr / b->chr));
 }
 
-static bool Char_eq(CharClass *this, CharClass *c)
+static bool Char_eq(const CharClass *a, const CharClass *b)
 {
-    if (!this || !c)
-        raise("Invalid argument");
-    return this->c == c->c;
+    if (a == NULL || b == NULL)
+        raise("Invalid char objects passed to eq");
+    return a->chr == b->chr ? true : false;
 }
 
-static bool Char_gt(CharClass *this, CharClass *c)
+static bool Char_gt(const CharClass *a, const CharClass *b)
 {
-    if (!this || !c)
-        raise("Invalid argument");
-    return this->c > c->c;
+    if (a == NULL || b == NULL)
+        raise("Invalid char objects passed to qt");
+    return a->chr > b->chr ? true : false;
 }
 
-static bool Char_lt(CharClass *this, CharClass *c)
+static bool Char_lt(const CharClass *a, const CharClass *b)
 {
-    if (!this || !c)
-        raise("Invalid argument");
-    return this->c < c->c;
+    if (a == NULL || b == NULL)
+        raise("Invalid char objects passed to lt");
+    return a->chr < b->chr ? true : false;
 }
 
 // Create additional functions here
@@ -102,7 +98,7 @@ static const CharClass _description = {
         .__size__ = sizeof(CharClass),
         .__name__ = "Char",
         .__ctor__ = (ctor_t)&Char_ctor,
-        .__dtor__ = NULL,   /* Not needed atm */
+        .__dtor__ = NULL,
         .__str__ = (to_string_t)&Char_str,
         .__add__ = (binary_operator_t)&Char_add,
         .__sub__ = (binary_operator_t)&Char_sub,
@@ -112,7 +108,7 @@ static const CharClass _description = {
         .__gt__ = (binary_comparator_t)&Char_gt,
         .__lt__ = (binary_comparator_t)&Char_lt
     },
-    .c = '\0',
+    .chr = '\0',
 };
 
 const Class *Char = (const Class *)&_description;
