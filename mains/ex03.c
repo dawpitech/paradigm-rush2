d1a811f922a0c178ee57935527b9b9833742a683
Antoine <me@antoinebellanger.fr>
2025-01-22 12:05:40 +0100

    feat(ex03): improve main to reduce memory leaks

--- mains/ex03.c
+++ mains/ex03.c
@@ -10,19 +10,23 @@
 #include "point.h"
 #include "vertex.h"
 
-int         main(void)
+int main(void)
 {
-    Object  *p1 = new(Point, 12, 13);
-    Object  *p2 = new(Point, 2, 2);
+    Object *p1 = new(Point, 12, 13);
+    Object *p2 = new(Point, 2, 2);
 
     printf("%s + %s = %s\n", str(p1), str(p2), str(addition(p1, p2)));
     printf("%s - %s = %s\n", str(p1), str(p2), str(subtraction(p1, p2)));
 
-    Object  *v1 = new(Vertex, 1, 2, 3);
-    Object  *v2 = new(Vertex, 4, 5, 6);
+    Object *v1 = new(Vertex, 1, 2, 3);
+    Object *v2 = new(Vertex, 4, 5, 6);
 
     printf("%s + %s = %s\n", str(v1), str(v2), str(addition(v1, v2)));
     printf("%s - %s = %s\n", str(v1), str(v2), str(subtraction(v1, v2)));
 
+    delete(v1);
+    delete(v2);
+    delete(p1);
+    delete(p2);
     return (0);
 }
