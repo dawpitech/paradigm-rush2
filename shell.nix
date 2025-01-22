2a9d8d44433e959405a38206e3bd8aed3b528664
Antoine <me@antoinebellanger.fr>
2025-01-22 08:58:04 +0100

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    packages = with pkgs; [
+      gcc
+      gnumake
+      valgrind
+      criterion
+      gdb
+      gcovr
+      hexyl
+      imhex
+      ncurses
+    ];
+}
